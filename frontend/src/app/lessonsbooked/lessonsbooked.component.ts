c7c83509bcb703b497b85d642daba6922cc56cc2
Giada <giadagabriele@outlook.com>
2021-12-16 09:08:42 +0100

    added lessons in mylessons

--- frontend/src/app/lessonsbooked/lessonsbooked.component.ts
+++ frontend/src/app/lessonsbooked/lessonsbooked.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 import {MenuItem} from 'primeng/api';
+import { BookLessonService } from '../booklesson.service';
 
 @Component({
   selector: 'app-lessonsbooked',
@@ -8,6 +10,20 @@ import {MenuItem} from 'primeng/api';
 })
 export class LessonsbookedComponent implements OnInit {
   items: MenuItem[] = [];
+  booking: any [] = [];
+
+  constructor(private bookLessonService: BookLessonService){
+    bookLessonService.getBookedLesson().subscribe(
+      (response) => {
+        console.log(response)
+        _.map(response, (el) => {
+          this.booking.push(el)
+        })
+      },
+      (error) => {console.log(error)},
+    )
+  }
+
   ngOnInit() {
       this.items = [
           {label:'Homepage', routerLink :"/homepage-student"},
